0c027bfc256f819ef9b8429f506788a7efe673f3
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-03-24 15:55:56 -0700

    Initial setup

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const volunteers = require("./routes/api/volunteers");
 const elders = require("./routes/api/elders");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (res, req) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+};
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -15,9 +24,15 @@ app.get("/", (req, res) => {
   res.send("Hello Protect our Elders!")
 });
 
+// Middleware
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
 app.use("/api/volunteers", volunteers);
 app.use("/api/elders", elders);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
