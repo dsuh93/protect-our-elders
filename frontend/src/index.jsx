71fa52b0f031b459476b710e55a75b50f5902c32
gscarborough <gigimscarborough@gmail.com>
2021-03-23 23:29:47 -0400

    added event lister dom cntn to index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import axios from 'axios';
+// import App from './App';
+// import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-window.axios = axios;
+// window.axios = axios;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+// Render our root component and pass in the store as a prop
+const root = document.getElementById('root');
+
+ReactDOM.render(<Root store={store} />, root);
+});
