1069dde49d0880e1a3190a779944a4875a6fbb77
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-03-27 12:50:57 -0700

    Fix run dev error

--- routes/api/elders.js
+++ routes/api/elders.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = requre("passport");
+
+//Models
+const Elders = require("../../models/Elder");
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the elder route"});
