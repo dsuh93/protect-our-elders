3b9b42512d7fd9af81ad01fdf992c30f6edfe09f
berthornhill <64211468+berthornhill@users.noreply.github.com>
2021-04-04 10:51:51 -0400

    added component for about page and some html structure, place holder material

---
+++ frontend/src/components/about/about.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+
+const About = () => {
+
+    return (
+        <div className="about-outer">
+            <div className="about-inner">
+                <div className="about-head">
+                    <div className="about-title">About the Project</div>
+                </div>
+                <div className="about-content">
+                    <div className="about-text"><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
+                    <div className="about-image"><img src="frontend/src/assets/images/chinatown.jpg" alt="Chinatown-NYC"/></div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default About
--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './navbar/navbar';
 import Splash from './splash/splash';
+import About from './about/about'
 
 const App = () => (
     <div id="app-div">
         
             <Route path="/" component={Navbar} />
         <Switch>
+            <Route path="/about" component={About} />
             <Route exact path="/" component={Splash} />
          </Switch>
     </div>
