187a7669f2431f1ff54e40deb3a3e2698c379b55
KaiZhu <zhukai0803@gmail.com>
2021-04-04 14:38:19 -0400

    fix passport line 5 to admins

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const mongoose = require("mongoose");
-const Admin = mongoose.model("Admin"); //change this to Admin or User
+const Admin = mongoose.model("admins"); //change this to Admin or User
 const keys = require("../config/keys");
 
 const options = {};
