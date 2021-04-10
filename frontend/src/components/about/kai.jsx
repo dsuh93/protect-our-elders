6314cfa27679002f3fe6d150e82a521575568e61
gscarborough <gigimscarborough@gmail.com>
2021-04-09 23:58:28 -0400

    revert

--- frontend/src/components/about/kai.jsx
+++ frontend/src/components/about/kai.jsx
@@ -7,8 +7,7 @@ function kai (props) {
         github: "https://github.com/henryzihaohuang",
         angellist: "https://angel.co/u/henry-zihao-huang"
       };
-  
-      
+    
     return (
       <div className="about-card-container">
         <div className="about-card-subcontainer">
