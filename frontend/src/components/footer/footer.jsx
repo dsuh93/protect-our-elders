192bbc7ac3df54f79b884dad90b3dbe8359385c2
gscarborough <gigimscarborough@gmail.com>
2021-04-10 20:43:48 -0400

    lang popup complete

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Footer = (props) => {
+
+    const languages = ['English', 'Spanish', 'Mandarin', 'Cantonese', 'Taiwanese', 'Korean', 'Japanese', 'Tagalog', 'Vietnamese']
+
+
     return (
         <section className="footer-container">
             <footer>
                 <div className="footer-links">
                     <Link>Admin Login</Link>
-                    <ul className="lang-links">
-                        <li>Languages:&nbsp;</li>
-                        <li><a>English&nbsp;|&nbsp;</a></li>
-                        <li><a>Spanish&nbsp;|&nbsp;</a></li>
-                        <li><a>Mandarin&nbsp;|&nbsp;</a></li>
-                        <li><a>Cantonese&nbsp;|&nbsp;</a></li>
-                        <li><a>Taiwanese&nbsp;|&nbsp;</a></li>
-                        <li><a>Korean&nbsp;|&nbsp;</a></li>
-                        <li><a>Japanese&nbsp;|&nbsp;</a></li>
-                        <li><a>Tagalog&nbsp;|&nbsp;</a></li>
-                        <li><a>Vietnamese</a></li>
-                    </ul>
+                    <div className="dropup">
+                        <ul className="lang-popup">
+                            {languages.map(lang => <li>{lang}</li>)}
+                        </ul>
+                        <i className="fas fa-language"></i>
+                    </div>
                 </div>
                 <p id="footer-copyright">
                     Copyright &copy; 2021 Protect Our Elders
