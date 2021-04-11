f4d2a2037c024f593299f5e2898576df146c895c
gscarborough <gigimscarborough@gmail.com>
2021-04-11 17:48:48 -0400

    add to about and started building out other links

---
+++ frontend/src/components/feedback/feedback.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Feedback = (props) => {
+
+
+    return (
+        <div className="get-involved-cont">
+            <h1>Feedback</h1>
+                <span className="form-btn">Click Here</span>
+            <h1>Incident Report</h1>
+                <span>If you are a volunteer who has experienced or witnessed something disturbing during your shift, please file an incident report below.</span>
+                <span className="form-btn">Click Here</span>
+        </div>
+    )
+}
+
+export default Feedback;
---
+++ frontend/src/components/protection/protection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Protection = (props) => {
+
+
+    return (
+       <div className="get-involved-cont">
+           <h1>Are There Elders That Need Protection In Your Area?</h1>
+            <span>We want to help you keep your community safe! If you are hoping 
+                to expand our efforts and start a new Protection project in your 
+                own area, please fill out the form below and we will get back to 
+                you soon!
+            </span>
+            <span className="form-btn">Inquire Here</span>
+       </div>
+    )
+}
+
+export default Protection;
