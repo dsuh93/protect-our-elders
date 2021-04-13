01faaf1ffa24e395571b48043d17aac41b57e3ce
dsuh93 <davidisuh93@gmail.com>
2021-04-13 18:02:51 -0400

    add skeleton for admin page

---
+++ frontend/src/components/admin/Admin.jsx
@@ -0,0 +1,15 @@
+function Admin() {
+  // admin page should include access to reservations made by elders and availability set by volunteers
+  // when clicking on a volunteers, should be able to see calendar highlighted with availability
+  // when clicking on an elder, should be able to see calendar highlighted with requested date/time
+  //include a sort option by date/time
+  // elder/volunteer cards should have all relative details on them, or at least name and contact info
+  return (
+    <>
+    {/* header for admin page, if admin has id, then have Hello 'id' in the top right corner */}
+    {/* have page split with top half split left and right calendars, bottom section should be platform to contact both elder and volunteer matched with each other */}
+    </>
+  )
+}
+
+export default Admin;
