eb710b04597a80b0e7026234ab46e7204a2f4f7f
David <davidjhinku@gmail.com>
2021-05-02 13:15:35 -0400

    Merged specific volunteer section

--- frontend/src/components/volunteers/volunteers.jsx
+++ frontend/src/components/volunteers/volunteers.jsx
@@ -9,11 +9,27 @@ const Volunteers = (props) => {
             <h1>General Volunteer Application</h1>
             <p className="strong">All chaperones must fill out this application.</p>
             <span className="form-btn">Click Here</span>
+            
             <h1>Specific Volunteer Role Application</h1>
-            <p ><strong>Chaperone Dispatcher (remote):</strong>Must be fluent in Cantonese, Mandarin, or Vietnamese.</p>
-            <p className="strong">Chaperone Dispatcher (remote):</p>
-            <p className="strong">Volunteer Coordinator</p>
+            <ul>
+                <li>
+                    <span className='strong'>Volunteer Dispatch (remote):</span> If organization is your thing, we would love your help matching volunteer's schedules with seniors requesting a chaperone. Must be fluent in Cantonese, Mandarin, or Vietnamese.
+                </li>
+                <li>
+                    <span className='strong'>Community Outreach:</span> Communities are stronger when we work together. Reach out to local business' and help build out a network our seniors can rely on.
+                </li>
+                <li>
+                        <span className='strong'>Copywriting (remote):</span> We're just getting started, and we need help keeping the community updated on what's going on.
+                </li>
+                <li>
+                    <span className='strong'>Social Media (remote):</span> Help spread the word and think through new avenues to reach target communities in need of assistance.
+                </li>
+                <li>
+                    <span className='strong'>Admin:</span> There's a lot to be done behind the scenes so reach out with your skillset and ideas on how you can apply them with us.
+                </li>
+            </ul>
             <span className="form-btn">Click Here</span>
+            
             <h1>COVID-19 Test Submission</h1>
             <p>For the safety of our volunteers and the community we are 
                 serving, we are asking that ALL volunteers doing in-person work 
@@ -21,30 +37,11 @@ const Volunteers = (props) => {
                 your results after completing an onboarding session.</p>
             <span className="form-btn">Click Here</span>
             
-            <h2>General Volunteer Application</h2>
-            <br/>
-            <p>Volunteers are at the heart of everything we do, and we appreciate your support protecting your local community!</p>
-            <br/>
-            <h3>Roles we're looking for</h3>
             <ul>
                 <li>
                     <span><strong>Chaperone:</strong> Throw on some sunscreen and your walking shoes. You'll be helping our seniors retain their independance and feel safe out in their neighboorhoods.</span>
                 </li>
-                <li>
-                    <span><strong>Volunteer Dispatch:</strong> If organization is your thing, we would love your help matching volunteer's schedules with seniors requesting a chaperone.</span>
-                </li>
-                <li>
-                    <span><strong>Community Outreach:</strong> Communities are stronger when we work together. Reach out to local business' and help build out a network our seniors can rely on.</span>
-                </li>
-                <li>
-                    <span><strong>Copywriting:</strong> We're just getting started, and we need help keeping the community updated on what's going on.</span>
-                </li>
-                <li>
-                    <span><strong>Social Media:</strong> Help spread the word and think through new avenues to reach target communities in need of assistance.</span>
-                </li>
-                <li>
-                    <span><strong>Admin:</strong> There's a lot to be done behind the scenes so reach out with your skillset and ideas on how you can apply them with us.</span>
-                </li>
+                
             </ul>
             {/* <Link className="form-link" to='/volunteers/signup'><button className="volunteer-signup-btn">Become a Volunteer</button></Link> */}
 
