71d62f43a40c3d0513224d6a32befb9f721871ca
gscarborough <gigimscarborough@gmail.com>
2021-05-02 12:37:08 -0400

    volunteers page

--- frontend/src/components/volunteers/volunteers.jsx
+++ frontend/src/components/volunteers/volunteers.jsx
@@ -6,6 +6,19 @@ const Volunteers = (props) => {
     return (
         <div className="volunteer-cont">
             <h1>General Volunteer Application</h1>
+            <p className="strong">All chaperones must fill out this application.</p>
+            <span className="form-btn">Click Here</span>
+            <h1>Specific Volunteer Role Application</h1>
+            <p ><strong>Chaperone Dispatcher (remote):</strong>Must be fluent in Cantonese, Mandarin, or Vietnamese.</p>
+            <p className="strong">Chaperone Dispatcher (remote):</p>
+            <p className="strong">Volunteer Coordinator</p>
+            <span className="form-btn">Click Here</span>
+            <h1>COVID-19 Test Submission</h1>
+            <p>For the safety of our volunteers and the community we are 
+                serving, we are asking that ALL volunteers doing in-person work 
+                to get tested for COVID-19 and to submit a screenshot/photo of 
+                your results after completing an onboarding session.</p>
+            <span className="form-btn">Click Here</span>
             
         </div>
     )
