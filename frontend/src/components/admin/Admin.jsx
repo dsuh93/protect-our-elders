52d3323056927f679198b6a0f03024a82378381e
dsuh93 <davidisuh93@gmail.com>
2021-05-02 12:48:59 -0400

    add header and details with summary for volunteers and requests

--- frontend/src/components/admin/Admin.jsx
+++ frontend/src/components/admin/Admin.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Admin() {
   // admin page should include access to reservations made by elders and availability set by volunteers
   // when clicking on a volunteers, should be able to see calendar highlighted with availability
@@ -6,7 +8,19 @@ function Admin() {
   // elder/volunteer cards should have all relative details on them, or at least name and contact info
   return (
     <>
-    {/* header for admin page, if admin has id, then have Hello 'id' in the top right corner */}
+      <h1>Admin Page</h1>
+      <h6></h6>
+      <br/>
+      <div>
+        <details className="admin volunteer-details">
+          <summary>Volunteers</summary>
+          <p>This section is dedicated to showing availability for volunteers.</p>
+        </details>
+        <details className="admin request-details">
+          <summary>Requests</summary>
+          <p>This section is dedicated to showing community member's requests for chaperones.</p>
+        </details>
+      </div>
     {/* have page split with top half split left and right calendars, bottom section should be platform to contact both elder and volunteer matched with each other */}
     </>
   )
