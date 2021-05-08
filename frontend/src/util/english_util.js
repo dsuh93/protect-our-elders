13e0f87a5a707a3861b4781e4cc7a444bac98db7
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-05-07 22:10:18 -0500

    Place translations for En/ZhCn/ZhTW

--- frontend/src/util/english_util.js
+++ frontend/src/util/english_util.js
@@ -26,6 +26,9 @@ const english = {
     email: 'Email',
     message: 'Message',
     sendMessage: 'Send Message',
+    softwareEngineer: "Software Engineer",
+    aboutUsBio:
+      "On October 26, 2020, seven aspiring developers set out on a journey that would change their lives forever. Equipped only with knowledge of how to iterate through an array and a dream, we took our places in App Academy's most illustrious cohort, and forged a path through the unknown, determined to conquer the world of software engineering. We learned, we laughed, we sometimes cried and developed applications to commemorate our pain in the form of memes. Most importantly, we crafted bonds of friendship and camaraderie that would last a lifetime. We collectively were, and still are, greatly affected by the rise of targeted violence against the AAPI community during the pandemic. Violence against anyone, especially the most vulnerable and revered members of our community, irrespective of their race, ethnicity, or other identifying characteristics, is both morally repugnant and reprehensible. With this project, we aim to take a stand and employ our development skills for good. We hope to donate this application to any person or organization with the desire and means to assume responsibility for what we've built. If this is something you might be interested in please do not hesitate to contact us. Until then, spread love not hate."
 }
 
 export default english
